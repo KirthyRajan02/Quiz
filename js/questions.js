15cc5fcfec8dc2c901d19715b411bff2c49d27a7
Kirthy Rajan <130735790+KirthyRajan02@users.noreply.github.com>
2024-03-22 11:02:36 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,131 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+  {
+    numb: 1,
+    question: "Software project manager is engaged with software management activities. He is responsible for",
+    answer: "All mentioned above",
+    options: ["Project planning", "Monitoring the progress", "Communication among stakeholders", "All mentioned above"],
+  },
+  {
+    numb: 2,
+    question: "Which of the following is not a characteristic of a software project",
+    answer: "Routine tasks are involved",
+    options: [ "Project is complex and large" ,"Routine tasks are involved","Project has a predetermined time span" ,"Project is carried out for others wareproject"],
+  },
+  {
+    numb: 3,
+    question: "Choose the correct order of activities covered by software project management ",
+    answer: "Feasibility, Planning, Design and Execution",
+    options: ["Planning, Analysis, Feasibility and Execution", "Requirement elicitation, Analysis, Planning and Execution", "Feasibility, Planning, Design and Execution", "Execution, Planning, Feasibility and Design"],
+  },
+  {
+    numb: 4,
+    question: "Who identified the characteristics to differentiate between s/w projects and other type of projects?",
+    answer: "Fred brooks",
+    options: ["Fred brooks", "Bob Hughes", "Mike Cotrell", "None of the above"
+      ],
+  },
+  {
+    numb: 5,
+    question: "Which is the next step after PFD –Product Flow Diagram?",
+    answer: "Ideal activity network",
+    options: ["Product Breakdown Structure", "Work breakdown structure", "Ideal activity network", "None of the above"
+      ],
+  },
+  {
+    numb: 6,
+    question: "What should be done by the project manager to ensure that all work in the project is included?",
+    answer: "Create a WBS",
+    options: ["Create a contingency plan", "Create a risk management plan", "Create a WBS", "Create a scope statement" 
+     ],
+  },
+  {
+    numb: 7,
+    question: "Technique used to frame Objectives and Sub Objectives of a Project",
+    answer: "SMART – Specific Measurable Achievable Relevant Time constrained",
+    options: ["PBS –Product Breakdown Structure",
+       "PFD – Product Flow Diagram",
+      "SMART – Specific Measurable Achievable Relevant Time constrained",
+      "SMART – Special Measurable Accepted Real-time Task"
+     ],
+  },
+  {
+    numb: 8,
+    question: "What should a project manager do or follow to ensure clear boundaries for project completion?",
+    answer: "Completing a scope statement",
+    options: ["Scope verification",
+      "Completing a scope statement",
+      "scope definition",
+      "Risk management plan"
+      ],
+  },
+  {
+    numb: 9,
+    question: "What does the acronym SMART stand for?",
+    answer: "Specific, measurable, achievable, relevant, time constrained",
+    options: ["Suitable, measurable, actionable, relevant, time constrained",
+      "Specific, measurable, actionable, resourced, time constrained",
+      "Standardised, measurable, achievable, relevant, time constrained",
+      "Specific, measurable, achievable, relevant, time constrained"
+      ],
+  },
+  {
+    numb: 10,
+    question: "Which one of the following is an appropriate sequence of steps in project planning?"    ,
+    answer: "Develop product breakdown structure, develop activity network, estimate effort and allocate resource",
+    options: ["Develop product breakdown structure, develop activity network, estimate effort and allocate resource",
+      "Develop activity network, develop product breakdown structure, estimate effort and allocate resource",
+      "Develop product breakdown structure, estimate effort, develop activity network and allocate resource",
+      "Allocate resource, develop product breakdown structure, develop activity network and estimate effort"
+      ],
+  },
+  {
+    numb: 11,
+    question: "Actor-Technology – Structure – Task is factors of which of the following",
+    answer: "Socio technical Model of risk",
+    options: ["Project Plan",
+    "Corrective Action",
+    "Scope statement",
+    "Socio technical Model of risk"
+   ],
+  },
+  {
+    numb: 12,
+    question: "_________ is the difference between the total costs and the total income over the life of the project.",
+    answer: "Net profit",
+    options: ["Payback period",
+      "Net present value",
+      "Net profit",
+      "Internal rate of return"
+      ],
+  },
+  {
+    numb: 13,
+    question: "When the IRR is equal to the discount rate, what can be said about the NPV?",
+    answer: "It would be equal to zero",
+    options: [ "It would be positive",
+      "It would be equal to zero",
+      "It would be negative",
+      "It cannot be determined without knowing the exact discount rate"],
+  },
+  {
+    numb: 14,
+    question: "_________ merges several projects.",
+    answer: "Strategic programes",
+    options: ["Strategic programes",
+    "Business cycle programmes",
+    "Project manager",
+    "Benefits management"
+    ],
+  },
+  {
+    numb: 15,
+    question: "_________ manages one project at a time.",
+    answer: "Project manager",
+    options: ["Strategic programes",
+      "Business cycle programes",
+      "Project manager",
+      "Benefits management"
+      ],
+  },
+];
